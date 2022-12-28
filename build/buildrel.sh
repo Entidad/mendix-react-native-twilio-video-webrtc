d4f031d0a8bcedd4f723f018ea29299e73fd07c1
skullquake <ockert8080@gmail.com>
2022-12-28 19:52:46 +0200

    .

--- build/buildrel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ./dev
-cd ./android
-JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 ANDROID_SDK_ROOT=~/Android/Sdk/ ./gradlew -x lint build
--- build/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ./dev
-npm install --legacy-peer-deps
-npm install react-native-twilio-video-webrtc@2.0.0 --legacy-peer-deps
-#npm install react-native-twilio-video-webrtc@3.0.0 --legacy-peer-deps
